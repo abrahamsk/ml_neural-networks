e82c91e4d9b8fe936a08d0f2e77bdb78cd795b1b
abrahamsk <abrahamskatie@gmail.com>
2016-01-26 17:36:35 -0800

    Functions!

--- neural_net.py
+++ neural_net.py
@@ -18,10 +18,6 @@ import random
 # Network has 16 inputs (mapping to the 16 attributes of the letters in the data set)
 # one layer of hidden units, and 26 output units (for 26 classes of letters)
 
-################
-# Experiment 1 #
-################
-
 #################
 # hyperparameters
 #################
@@ -117,90 +113,12 @@ initial_weights = np.random.uniform(low= -.25, high= .25, size=(n,17) )
 #print Y.shape #4x26
 
 # weights from hidden layer to output layer
-# 5 columns to allow for bias input
+# 5 columns to allow for bias input (one column of 1s)
 hidden_to_output_weights = np.random.uniform(low= -.25, high= .25, size=(26,5) )
 
-# output matrix
+# output matrix, 1 row by 26 columns for 26 letters of the alphabet
+# only 1 row, only need one output (activations) for output layer
 # don't initialize to anything
 # target for properly identified letter is .9, and the rest of the units should be .1
 Y = np.full((1, 26), None)
 
-######
-# main
-######
-# run training examples through neural net to train for letter recognition
-# Classification with a two-layer neural network (Forward propagation)
-# For two-layer networks (one hidden layer):
-# I. For each test example:
-#     1. Present input to the input layer.
-#     2. Forward propagate the activations times the weights to each node in the hidden layer.
-#     3. Forward propagate the activations times weights from the hidden layer to the output layer.
-#     4. Interpret the output layer as a classification.
-epoch_increment = 0
-epoch = 5
-
-for iter in xrange(epoch):
-    text = "\rEpoch "+str((epoch_increment)+1)+"/"+str(epoch)
-    sys.stdout.write(text)
-
-    # for loops:
-    # training set: epoch
-        # input -> hidden layer
-        # hidden layer -> output layer
-
-    # iterate through data matrix to operate on individual training instances
-    for row in X[0:2]:
-        print "\n----New row in input matrix----"#, row #instance i vector
-        # transpose row vector for matrix multiplication
-        ##print row.shape
-        X_row = np.mat(row)
-        #print X_row.shape
-        X_col = X_row.transpose()
-        # # X[0,:][np.newaxis, :].T
-        # # X[0,:][None].T
-        ###print X_col.shape
-        ##print X_col
-
-        # forward propagation
-        # initial run of data, use sigmoid activation function
-        # pass in dot products of inputs and weights
-        hidden_layer = sigmoid(np.dot(initial_weights, X_col), False)
-        print hidden_layer # 4x1
-
-        # hidden_layer matrix is the activation at the hidden layer
-        # use hidden layer activations as input for the output layer
-
-    # use hidden layer activations to get activations for output layer
-    # for output_row in hidden_layer:
-    #     print output_row.shape
-    #     print "++++New row in output matrix++++"
-    #     Y_row = np.mat(output_row)
-    #     print Y_row.shape #1x26
-
-        ###output_layer = sigmoid(np.dot(hidden_to_output_weights, Y_row), False)
-        #print output_layer.shape #4x26
-
-    # use hidden layer activations to get activations for output layer
-    # append one row of 1s to hidden layer to allow for bias input
-    bias_input_hidden_layer = np.full((1, 1), 1.0)
-    #print bias_input_hidden_layer
-    hidden_layer_concat = np.concatenate((hidden_layer, bias_input_hidden_layer), axis=0)
-    #print hidden_layer_concat
-
-    # matrix multiply (hidden layer) dot (weights from hidden -> output)
-    output_layer = np.dot(hidden_to_output_weights, hidden_layer_concat)
-    ####print output_layer.shape #26x1
-
-    # apply sigmoid function to output layer
-    # to get activations at output layer
-    Y = sigmoid(output_layer, False)
-    ####print "output results", Y
-    ####print "Y shape", Y.shape #26x1
-
-        # calculate error
-
-        # change weights after each training example
-
-    epoch_increment += 1
-
-# list for target: list of 26 with one valued at .9 and the rest valued at .1
---
+++ train.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- # utf-8 for non-ASCII chars
+
+# Machine Learning 445
+# HW 2: Neural Networks
+# Katie Abrahams, abrahake@pdx.edu
+# 1/28/16
+
+# import data structures, variables, and neural net from neural_net
+from neural_net import *
+
+###############
+# function defs
+###############
+def feedforward():
+    """
+    Forward propagate the input through the neural network
+    Does not include error computation
+    :return: output of neural net
+    """
+    # iterate through data matrix to operate on individual training instances
+    for row in X[0:2]:
+        print "\n----New row in input matrix----"  # , row #instance i vector
+        # transpose row vector for matrix multiplication
+        ##print row.shape
+        X_row = np.mat(row)
+        # print X_row.shape
+        X_col = X_row.transpose()
+        # # X[0,:][np.newaxis, :].T
+        # # X[0,:][None].T
+        ###print X_col.shape
+        ##print X_col
+
+        # forward propagation
+        # initial run of data, use sigmoid activation function
+        # pass in dot products of inputs and weights
+        hidden_layer = sigmoid(np.dot(initial_weights, X_col), False)
+        print hidden_layer  # 4x1
+
+        # hidden_layer matrix is the activation at the hidden layer
+        # use hidden layer activations as input for the output layer
+
+    # use hidden layer activations to get activations for output layer
+    # append one row of 1s to hidden layer to allow for bias input
+    bias_input_hidden_layer = np.full((1, 1), 1.0)
+    # print bias_input_hidden_layer
+    hidden_layer_concat = np.concatenate((hidden_layer, bias_input_hidden_layer), axis=0)
+    # print hidden_layer_concat
+
+    # matrix multiply (hidden layer) dot (weights from hidden -> output)
+    output_layer = np.dot(hidden_to_output_weights, hidden_layer_concat)
+    ####print output_layer.shape #26x1
+
+    # apply sigmoid function to output layer
+    # to get activations at output layer
+    Y = sigmoid(output_layer, False)
+    ####print "output results", Y
+    ####print "Y shape", Y.shape #26x1
+
+    # return activation from output layers
+    return Y
+
+
+def train(num_epochs):
+    """
+    Run training examples through neural net to train for letter recognition
+    Classification with a two-layer neural network (Forward propagation)
+    For two-layer networks (one hidden layer):
+    # includes multiple epochs
+     I. For each test example:
+         1. Present input to the input layer.
+         2. Forward propagate the activations times the weights to each node in the hidden layer.
+         3. Forward propagate the activations times weights from the hidden layer to the output layer.
+         4. Interpret the output layer as a classification.
+    """
+    epoch_increment = 0
+
+    for iter in xrange(num_epochs):
+        text = "\rEpoch "+str((epoch_increment)+1)+"/"+str(num_epochs)
+        sys.stdout.write(text)
+
+        # for loops:
+        # training set: epoch
+            # input -> hidden layer
+            # hidden layer -> output layer
+
+        # feedforward input through neural net: input layer -> hidden layer -> output
+        # Y is the the output of the matrix, without any error correction
+        # but already processed through the sigmoid function
+        Y = feedforward()
+        #print "Post feedforward call", Y.shape #26x1
+        print "Post feedforward", Y
+
+        # calculate error
+        # o_k is output: 26 output
+        # t_k is target: per training instance: if input is A,
+        # then output for for matching node should be .9
+        # the rest of the outputs should be .1
+        # calculate delta_k for each output unit
+        # 2. Calculate the error terms:
+        #   For each output unit k, calculate error term δk :
+        #   δk ← ok(1 − ok)(tk − ok)
+        #
+        #   For each hidden unit j, calculate error term δj :
+        #   δj ← hj(1−hj) ( (∑ k∈output units) wkj δk )
+
+
+
+        # 3. change weights after each training example
+        # For each weight wkj from the hidden to output layer:
+        #   wkj ← wkj +Δwkj
+        #   where
+        #   Δwkj =ηδkhj
+        #
+        # For each weight wji from the input to hidden layer:
+        #   wji ←wji +Δwji
+        #   where
+        #   Δwji =ηδjxi
+
+
+        epoch_increment += 1
+
+    # list for target: list of 26 with one valued at .9 and the rest valued at .1
+
+################
+# Experiment 1 #
+################
+
+######
+# main
+######
+epochs = 5
+#train the neural net
+train(epochs)
